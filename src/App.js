3cb0cab09a6db480c9fe38c751ecb1b7fe109519
유진솔 <dbwlsthf90@korea.kr>
2022-11-09 15:51:00 +0900

    02-2 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) {
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+    </div>
+  );
+}
+
+const foodLike = [ {
+  name: 'Kimchi',
+  image: 'https://www.platingsandpairings.com/wp-content/uploads/2014/12/Kimchi-5.jpg',
+},
+{
+  name: 'Samgyeopsal',
+  image: 'https://trazy.s3.amazonaws.com/images/main/theme/cover/korean%20barbecue%20restaurant%20cover.jpg',
+},
+{
+  name: 'Bibimbap',
+  image: 'https://recipetineats.com/wp-content/uploads/2019/05/Bibimbap_3.jpg',
+},
+{
+  name: 'Doncasu',
+  image: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FySZaT%2FbtrKHZoIbWD%2FMGeTK0YKjBJoBidiGklXf0%2Fimg.jpg',
+},
+{
+  name: 'Kimbap',
+  image: 'https://curiouskimchi.com/wp-content/uploads/2022/02/LIM01948-min.jpeg',
+},
+];
+
+function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.image} />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  console.log(foodLike.map(renderFood));
+  
+  return ( <div>
+    {foodLike.map(renderFood)}
+   
     </div>
   );
 }
